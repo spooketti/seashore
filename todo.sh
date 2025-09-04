db042f764f04e8e3e51579b84a925b0d44dd4543
Jonathan Liu <jonliu168@gmail.com>
2025-09-04 10:28:21 -0700

    addcol rm col

--- todo.sh
+++ todo.sh
@@ -1,5 +1,22 @@
 cd "Programming/CLI/todo/.venv"
 source bin/activate
-cd "../"
-python main.py
-echo $1
+cd ../
+case $# in 
+   2) 
+      case $1 in 
+      	"addCol")
+	python main.py --addCol $2
+	;;
+        "rmCol")
+	python main.py --rmCol $2
+        ;;
+      esac
+      ;;
+   3)
+      echo 3
+      ;;
+   *) 
+      python main.py
+      ;;
+esac
+
--- todo/main.py
+++ todo/main.py
@@ -1,8 +1,28 @@
-from terminaltables import AsciiTable
+from prettytable import PrettyTable
 import json
-todoTable = {}
-with open("todo.json","r") as todo:
-    todoTable = json.load(todo)
+import argparse
 
-table = AsciiTable(todoTable["main"])
-print(table.table)
+table = PrettyTable()
+todoTable = {}
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-addCol","--addCol", type=str)
+parser.add_argument("-rmCol","--rmCol", type=str)
+args = parser.parse_args()
+
+with open("todo.json","r") as todo:
+   todoTable = json.load(todo)
+
+if(args.addCol):
+    todoTable[args.addCol] =  [""] * len(todoTable[list(todoTable.keys())[0]])
+    
+if(args.rmCol): 
+    del todoTable[args.rmCol]
+
+for title in todoTable:
+    table.add_column(title,todoTable[title])
+
+print(table)
+
+with open("todo.json", "w") as json_file:
+    json.dump(todoTable, json_file, indent=4)
